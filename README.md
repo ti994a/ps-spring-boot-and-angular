ae05285c00d35019096b5f12d348d2f132172874
joe <success1@gmail.com>
2019-02-24 20:52:41 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ps-spring-boot-and-angular
+
+## coursework from https://app.pluralsight.com/library/courses/building-first-app-with-spring-boot-angularjs/
+
+
---
+++ bike/src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -0,0 +1,37 @@
+package com.globomatics.bike.controllers;
+
+import com.globomatics.bike.models.Bike;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.globomatics.bike.repositories.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/bikes")
+public class BikesController {
+
+    @Autowired
+    private BikeRepository bikeRepository;
+
+    @GetMapping
+    public List<Bike> list() {
+        return bikeRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void create(@RequestBody Bike bike) {
+        bikeRepository.save(bike);
+    }
+
+    @GetMapping("/{id}")
+    public Bike get(@PathVariable("id") long id) {
+        return bikeRepository.getOne(id);
+    }
+
+}
+
